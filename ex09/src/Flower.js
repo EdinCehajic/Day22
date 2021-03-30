31708150caa0d9de1ad3dbfda2eefbe42c3c1295
EdinCehajic <ed.cehajic@gmail.com>
2021-03-30 09:31:21 +0200

    Adding

---
+++ ex09/src/Flower.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Flower extends Component {
+    render() { 
+        return ( <h2>This is a {} Flower!</h2>
+         );
+    }
+}
+ 
+export default Flower;
---
+++ ex10/src/Box.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+function Box(props) {
+    return (
+        <h2>This is a {} Box!</h2>
+    );
+}
+
+export default Box;
