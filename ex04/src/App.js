e2e1f943752727303ad0cd8a91ea8b3da7d16612
EdinCehajic <ed.cehajic@gmail.com>
2021-03-30 09:19:01 +0200

    Adding

---
+++ ex04/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import Box from './Box';
+import './App.css';
+
+function App() {
+  return (<div className="App"><Box /></div>);
+}
+
+export default App;
--- ex08/src/app.js
+++ ex08/src/app.js
@@ -1,13 +1,15 @@
 // src/App.js
 
 import React, { Component } from 'react';
+import Table from './Table';
+
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <h1>Hello, React!</h1>
+            <div className="container">
+                <Table />
             </div>
         )
     }
 }
-exports default App;
+export default App;
